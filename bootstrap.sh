e068855f62399f00f8ee193da01db3d9e2ed1854
psevdaisthisi <code@fmoreira.art>
2020-11-21 15:38:45 +0000

    Fix error logging

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,7 +5,7 @@ pushd "$script_path" > /dev/null
 
 printinfo "Installing dotfiles..."
 . install-dotfiles.sh ||
-{ printinfo "Installing dotfiles... [FAILED]"; exit 1; }
+{ printerr "Installing dotfiles... [FAILED]"; exit 1; }
 
 printinfo "Updating keyboard key repeat rate..."
 defaults write -g InitialKeyRepeat -int 12 &&
@@ -21,7 +21,7 @@ sudo spctl --master-disable ||
 printinfo "Installing Homebrew..."
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)" &&
 /usr/local/bin/brew doctor ||
-{ printinfo "Installing Homebrew... [FAILED]"; exit 1; }
+{ printerr "Installing Homebrew... [FAILED]"; exit 1; }
 
 
 printinfo "Installing Homebrew packages..."
@@ -31,31 +31,31 @@ _pkgs_tools=(aria2 bat bash bash-completion@2 coreutils dash entr exa fd ffmpeg
 _pkgs_dev=(cmake gcc gcc@8 gcc@9 git git-delta go grip llvm ninja node@12
            pkg-config python tig)
 /usr/local/bin/brew install ${_pkgs_tools[*]} ${_pkgs_dev[*]} ||
-{ printinfo "Installing Homebrew packages... [FAILED]"; exit 1; }
+{ printerr "Installing Homebrew packages... [FAILED]"; exit 1; }
 
 
 printinfo "Installing PIP packages..."
 /usr/local/bin/pip3 install --user compiledb pynvim ||
-{ printinfo "Installing PIP packages... [FAILED]"; exit 1; }
+{ printerr "Installing PIP packages... [FAILED]"; exit 1; }
 
 
 printinfo "Installing Neovim plugins..."
 /usr/local/bin/nvim +PlugInstall +qa &&
 /usr/local/bin/nvim +CocUpdateSync +qa ||
-{ printinfo "Installing Neovim plugins... [FAILED]"; exit 1; }
+{ printerr "Installing Neovim plugins... [FAILED]"; exit 1; }
 
 printinfo "Installing SF Mono Powerline font..."
 curl -L 'https://github.com/Twixes/SF-Mono-Powerline/archive/v16.0d1e1.zip' -o sfmono-powerline.zip &&
 unzip -j sfmono-powerline.zip -d sfmono-powerline &&
 cp sfmono-powerline/*.otf /Users/psevdaisthisi/Library/Fonts/ &&
 rm -rf sfmono-powerline* ||
-{ printinfo "Installing Neovim plugins... [FAILED]"; exit 1; }
+{ printerr "Installing Neovim plugins... [FAILED]"; exit 1; }
 
 [ -z "$(grep fish /etc/shells)" ] && {
 	printinfo "Changing default shell to Fish..."
 	echo "/usr/local/bin/fish" | sudo tee -a /etc/shells &&
 	chsh -s /usr/local/bin/fish psevdaisthisi ||
-	{ printinfo "Installing Neovim plugins... [FAILED]"; exit 1; }
+	{ printerr "Installing Neovim plugins... [FAILED]"; exit 1; }
 }
 
 popd > /dev/null
